2e6e88e6c91c7bff92f3b8d69b19453ea4eee2e5
Lukasz Patalas <lukasz.patalas86@gmail.com>
2023-03-20 22:57:50 +0000

    Change layout and implement gpt-3.5-turbo
    Great news! I was able to successfully implement the gpt-3.5-turbo
    model, but not without encountering some errors initially. However,
    after doing some investigation, I discovered that the issue was due to
    using an outdated version of the open ai model. Once I updated to the
    current model version, everything started to work smoothly. Of course,
    there is still some work to be done, such as tweaking the model's
    parameters and ensuring that the prompts are optimized, but so far the
    results are looking promising.

--- app.py
+++ app.py
@@ -10,26 +10,31 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        animal = request.form["animal"]
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
+        text = request.form["text"]
+        prompt = generate_prompt(text)
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": prompt},
+            ],
+            temperature=0.7,
+            max_tokens=150,
+            top_p=1.0,
+            frequency_penalty=0.0,
+            presence_penalty=0.0
         )
-        return redirect(url_for("index", result=response.choices[0].text))
+        generated_text = response["choices"][0]["message"]["content"]
+        generated_text = generated_text.strip()
+        return redirect(url_for("index", result=generated_text))
 
     result = request.args.get("result")
     return render_template("index.html", result=result)
 
 
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
+def generate_prompt(text):
+    return f"""Please rephrase the following text in a way that is clear, balanced, and understandable by a non-technical audience with a touch of creativity:
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
+{text}""".format(
+        text
     )
